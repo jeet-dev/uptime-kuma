c240b83005720b32c42b51bd4abccdeb87a1b69c
jeet-dev <jeet.devapps@gmail.com>
2022-02-12 13:55:17 -0500

    Update StatusPage.vue

--- src/pages/StatusPage.vue
+++ src/pages/StatusPage.vue
@@ -208,7 +208,7 @@
         </div>
 
         <footer class="mt-5 mb-4">
-            {{ $t("Powered by") }} <a target="_blank" href="https://github.com/louislam/uptime-kuma">{{ $t("Uptime Kuma" ) }}</a>
+            {{ $t("Powered by") }} <a target="_blank">{{ $t("Uptime Apps" ) }}</a>
         </footer>
     </div>
 </template>
